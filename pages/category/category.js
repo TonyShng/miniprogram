44790539db0df86492508dc47d092c25f36cb90d
RainTwo <wangzhe92824@qq.com>
2022-01-26 17:13:04 +0800

    首页 分类页

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,23 +1,159 @@
 // pages/category/category.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
+    cateItems: [{
+      cate_id: 1,
+      cate_name: "英短",
+      ishaveChild: true,
+      children: [{
+        txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+        img: '/utils/fakerImage/banner.jpg',
+        up1: '2300',
+        up2: '1300',
+        isup: '1'
+      }]
+    }, {
+      cate_id: 2,
+      cate_name: "美短",
+      ishaveChild: true,
+      children: [{
+        txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+        img: '/utils/fakerImage/banner.jpg',
+        up1: '2300',
+        up2: '1300',
+        isup: '1'
+      }]
+    }, {
+      cate_id: 3,
+      cate_name: "波斯",
+      ishaveChild: false,
+      children: []
+    }, {
+      cate_id: 4,
+      cate_name: "加菲",
+      ishaveChild: true,
+      children: [{
+        txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+        img: '/utils/fakerImage/banner.jpg',
+        up1: '2300',
+        up2: '1300',
+        isup: '1'
+      }]
+    }, {
+      cate_id: 5,
+      cate_name: "布偶",
+      ishaveChild: true,
+      children: [{
+        txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+        img: '/utils/fakerImage/banner.jpg',
+        up1: '2300',
+        up2: '1300',
+        isup: '1'
+      }]
+    }, {
+      cate_id: 6,
+      cate_name: "暹罗",
+      ishaveChild: true,
+      children: [{
+        txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+        img: '/utils/fakerImage/banner.jpg',
+        up1: '2300',
+        up2: '1300',
+        isup: '1'
+      }]
+    }, {
+      cate_id: 7,
+      cate_name: "缅因",
+      ishaveChild: true,
+      children: [{
+        txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+        img: '/utils/fakerImage/banner.jpg',
+        up1: '2300',
+        up2: '1300',
+        isup: '1'
+      }]
+    }, {
+      cate_id: 8,
+      cate_name: "狸花",
+      ishaveChild: true,
+      children: [{
+        txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+        img: '/utils/fakerImage/banner.jpg',
+        up1: '2300',
+        up2: '1300',
+        isup: '1'
+      }]
+    }],
+    curNav: 1,
+    curIndex: 0
+  },
 
+  switchRightTab: function(e) {
+    let id = e.target.dataset.id,
+    index = parseInt(e.target.dataset.index);
+    this.setData({
+      curNav: id,
+      curIndex: index
+    })
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
+  onLoad: function (options) {
+    
+  },
 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,28 +1,164 @@
 // pages/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    bannerWidth: 335,
+    bannerHeight: 250,
+    itemWidth: 160,
+    itemHeight: 160,
+    itemBorderWidth: 10,
     indicatorDots: true,
-    vertical: false,
-    autoplay: false,
-    interval: 2000,
-    duration: 500
+    indicatorColor: "white",
+    activeColor: "coral",
+    autoplay: true,
+    circular: true,
+    interval: 3000,
+    duration: 3000,
+    imgUrls: [{
+      link: '',
+      url: '/utils/fakerImage/top1.jpeg'
+    }, {
+      link: '',
+      url: '/utils/fakerImage/top2.jpg'
+    }, {
+      link: '',
+      url: '/utils/fakerImage/top3.jpg'
+    }
+    ],
+    array: [{
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }, {
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }, {
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }, {
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }, {
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }, {
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }, {
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }, {
+      txt: '英短银渐层 1岁 已驱虫 已打疫苗 未绝育',
+      img: '/utils/fakerImage/banner.jpg',
+      up1: '2300',
+      up2: '1300',
+      isup: '1'
+    }]
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
+  onLoad: function (options) {
+    var self = this;
+    wx.setNavigationBarTitle({
+      title: '首页',
+    });
 
+    wx.getSystemInfo({
+      success: (result) => {
+        var widths = result.windowWidth;
+        var bfb = widths * 1.0 / 187;
+        if (bfb <= 1) {
+          bfb = 1;
+        }
+        if (widths < 700) {
+          bfb = 2;
+        }
+        var num = Math.floor(bfb) * 2 + 2;
+        var numtol = Math.floor(num * 4);
+        var panlwidth = widths - numtol;
+        console.log('dd: ' + panlwidth);
+        self.setData({
+          bannerWidth: Math.floor(widths),
+          bannerHeight: Math.floor(widths / 335 * 250),
+          itemWidth: Math.floor(panlwidth * 1.0 / Math.floor(bfb)),
+          itemHeight: Math.floor(panlwidth * 1.0 / Math.floor(bfb)),
+          itemBorderWidth: 4
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
